680cdd417fdf70ffa219df733d9e6111bd61c356
waleedx7 <w.alshelahi@gmail.com>
2023-01-14 19:05:01 +0300

    done by waleed

--- functions.py
+++ functions.py
@@ -1,24 +1,24 @@
 from books import books
 
-print(books[0])
+#print(books[0])
 #  number_of_authors(book)
 #  recieves a book dictionary
 #  returns the number of authors that the book has
 def number_of_authors(book):
-    ...
+    return len(book['authors'])
 
-
-print(number_of_authors(books[0]))
+# print(number_of_authors(books[4]))
 
 #  get_book_by_id(book_id, books)
 #  # receives a book id
 #  # recieves a list of book dictionaries
 #  # returns the book dictionary with the same id as the book_id provided
 def get_book_by_id(book_id, books):
-    ...
+    for book in books:
+        if book_id == book['id']:
+         return book
 
-
-print(get_book_by_id(38, books))
+# print(get_book_by_id(38, books))
 
 
 # add_summary_to_book(summary, book)
@@ -27,10 +27,11 @@ print(get_book_by_id(38, books))
 # adds the summary to the book dictionary
 # return the book dictionary
 def add_summary_to_book(summary, book):
-    ...
+    book['Summary'] = summary
+    return book
 
 
-print(add_summary_to_book("this is a good book about", books[0]))
+# print(add_summary_to_book("this is a good book about", books[0]))
 
 
 # CHALLENGE 1
@@ -41,11 +42,10 @@ print(add_summary_to_book("this is a good book about", books[0]))
 
 
 def get_book_property(property, book):
-    ...
-
+    return book [property]
 
 print(get_book_property("color", books[0]))
-print(get_book_property("title", books[0]))
+print(get_book_property("title", books[1]))
 
 
 # CHALLENGE 2
@@ -55,18 +55,20 @@ print(get_book_property("title", books[0]))
 
 
 def calculate_not_available_books(books):
-    ...
+    return [book for book in books if not book["available"]]
 
 
-print(calculate_not_available_books(books))
+# print(calculate_not_available_books(books))
 
 # CHALLENGE 3
 # get_book_by_author_name(author_name, books)
 # receives a author name (string)
 # recieves a list of book dictionaries
 # returns the book dictionary that contains an author with the author name provided
+
 def get_book_by_author_name(author_name, books):
-    ...
-
-
+    for book in books:
+        if author_name == book['authors']:
+          return book
+#not working :(
 print(get_book_by_author_name("Neil Gaiman", books))
